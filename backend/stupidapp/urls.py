7073feefa82ada91c5e0074f776f414362001ebd
Aditya Hase <aditya@adityahase.com>
2018-03-16 16:08:10 +0530

    Add required model classes and a generic view to see them all

--- backend/stupidapp/urls.py
+++ backend/stupidapp/urls.py
@@ -1,8 +1,15 @@
 from django.urls import include, path
 
-from .views import show, submit
+from .views import show, submit, classes
 
 urlpatterns = [
     path('show/', show),
     path('submit/', submit),
+    path('campaigns/', classes, {"class": "Campaign"}),
+    path('children/', classes, {"class": "Child"}),
+    path('courses/', classes, {"class": "Course"}),
+    path('mothers/', classes, {"class": "Mother"}),
+    path('organizations/', classes, {"class": "Organization"}),
+    path('vaccines', classes, {"class": "Vaccine"}),
+    path('immunizations/', classes, {"class": "Immunization"}),
 ]
--- backend/stupidapp/views.py
+++ backend/stupidapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST, require_GET
+from .models import Campaign, Child, Course, Immunization, Mother, Organization, Vaccine
 import json
 
 # Create your views here.
@@ -16,3 +17,20 @@ def submit(request):
     if "message" in data:
         return JsonResponse({"status": "success", "message": data["message"]})
     return JsonResponse({"staus":"failure", "message": None})
+
+@require_GET
+def classes(request, **kwargs):
+    klass = {
+        "Campaign": Campaign,
+        "Child": Child,
+        "Course": Course,
+        "Immunization": Immunization,
+        "Mother": Mother,
+        "Organization": Organization,
+        "Vaccine": Vaccine
+    }
+    try:
+        response = JsonResponse({"status": "success", "data": list(klass[kwargs["class"]].objects.all())}, status=200)
+    except:
+        response = JsonResponse({"status": "failure"}, status=404)
+    return response
