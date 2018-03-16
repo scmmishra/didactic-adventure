d176da399679ab61a845f4b71828e74f227c4380
Aditya Hase <aditya@adityahase.com>
2018-03-16 11:51:23 +0530

    Use /api/... urls for using api

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("stupidapp.urls")),
+    path('api/', include("stupidapp.urls")),
 ]
